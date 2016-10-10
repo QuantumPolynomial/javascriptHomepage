0c1389c931364f63203cb573b35aa900c88dc98c
Henrik <henrik.ahlkvist@gmail.com>
2016-10-10 20:24:35 +0200

    added the basics

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var clock = document.getElementById("clock");
+var hexColor = document.getElementById("hex-color");
+
+
+
+function hexClock() {
+    var time = new Date();
+    var hours = time.getHours().toString();
+    var minutes = time.getMinutes().toString();
+    var seconds = time.getSeconds().toString();
+
+	if (hours.length < 2){
+		hours = '0' + hours;
+	}
+	if (minutes.length < 2){
+		minutes = '0' + minutes;
+	}
+	if (seconds.length < 2){
+		seconds = '0' + seconds;
+	}
+
+    var hexColorStr = '#' + hours + minutes + seconds;
+	var clockStr =  hours + ':' + minutes + ':' + seconds;
+	hexColor.textContent = hexColorStr;
+	clock.textContent = clockStr;
+	document.body.style.color = hexColorStr;
+}
+hexClock()
+setInterval(hexClock, 1000);
